baa6e62c541cb584229117d12923f3c1f06266c2
Sarah Lorenzen <sarah@slorenzen.com>
2025-02-15 15:46:52 -0800

    Initial Build out, Add images

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// Word List
+const wordList = [
+  {
+    word: "code",
+    hint: "typed set of instructions that's fun!",
+  },
+  {
+    word: "javascript",
+    hint: "code that adds functionality",
+  },
+  {
+    word: "html",
+    hint: "code that provides structure",
+  },
+  {
+    word: "css",
+    hint: "code that adds design",
+  },
+  {
+    word: "hexadecimal",
+    hint: "code for colors",
+  },
+];
+
+// Game Variables
+const wordDisplay = document.querySelector(".word");
+const wordHint = document.querySelector(".hint");
+const lettersGuessed = document.querySelector(".guesses-total");
+const lettersKeyboard = document.querySelector(".keyboard");
+const partsHangman = document.querySelector(".hangman-display img");
